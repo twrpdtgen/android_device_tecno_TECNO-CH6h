8c0aeb58444f8062507017f8920c6f194864d3df
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-28 15:55:31 +0200

    TECNO-CH6h: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO CH6h Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-CH6h.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-CH6h-user \
+    omni_TECNO-CH6h-userdebug \
+    omni_TECNO-CH6h-eng
